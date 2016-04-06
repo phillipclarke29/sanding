b72318e4e0ae6ffb843ca6b22ad3a1b53a301f8a
philip <philip.clarke@redhood.co.uk>
2016-04-06 11:32:39 +0100

    changed server back

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 // index page
-app.get('/home', function(req, res) {
+app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
